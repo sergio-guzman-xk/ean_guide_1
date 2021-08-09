eed5fc6cfd5ebbaa89eec370b593368d4f0a308d
sgioguzman <sgioguzman@gmail.com>
2021-08-09 00:14:26 -0500

    fixing an error with the last repo. Did not include one package

--- src/com/company/actividad2/Actividad2.java
+++ src/com/company/actividad2/Actividad2.java
@@ -4,4 +4,5 @@ public class Actividad2 {
     /**
      * Please go to https://github.com/sergio-guzman-xk/guide1_act2. I had to move the answer to a different repo due to the main method already existing in this one.
      */
+    private String placeholder;
 }
