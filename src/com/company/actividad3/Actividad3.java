f07213e5f26ec06fdd159cba3d2dc7f914ca4f4c
sgioguzman <sgioguzman@gmail.com>
2021-08-09 01:15:14 -0500

    3act completed

---
+++ src/com/company/actividad3/Actividad3.java
@@ -0,0 +1,11 @@
+package com.company.actividad3;
+
+public class Actividad3 {
+    /**
+     * Please go to https://github.com/sergio-guzman-xk/guide1_act3. Mensaje para el tutor: Este punto lo entregue despues de la hora pactada.
+     * He tenido problemas con la declaracion de renta que vence este 17 y perdi la cabeza sobre esta entrega. Mil disculpas.
+     * Entiendo si este punto no es tenido en cuenta para la calificacion de la guia, pero tambien agradezco si puede ser tenido en cuenta con una calificacion
+     * mas baja de lo normal.
+     */
+    private String placeholder;
+}
